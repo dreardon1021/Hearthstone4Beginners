cb52656df9c350263e021c591e11cd8f42639950
Dan Reardon <dreardon1021@gmail.com>
2020-04-15 19:16:35 -0600

    Change class to className

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -4,11 +4,11 @@ import './Nav.css';
 const Nav = () => {
   return (
     <nav>
-      <div class="header-container">
-        <img class="header-logo" src="../../images/LogoHS.png" alt="hearthstone-logo"/>
+      <div className="header-container">
+        <img className="header-logo" src="../../images/LogoHS.png" alt="hearthstone-logo"/>
         <h1>Deck Creator For Beginners</h1>
       </div>
-      <button class="nav-button">View Current Deck</button>
+      <button className="nav-button">View Current Deck</button>
     </nav>
   )
 }
