27afccac3c44ae4b303201520bb765ae5251a761
Dan Reardon <dreardon1021@gmail.com>
2020-04-15 19:55:31 -0600

    Restructure file tree and setup redux store

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+export const loadClassicCards = cards => ({
+  type: 'LOAD_CLASSIC',
+  cards
+})
---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SideBar.css';
+import Form from '../Form/Form'
+
+const SideBar = () => {
+  return(
+    <aside>
+      <Form />
+    </aside>
+  )
+}
+
+export default SideBar
