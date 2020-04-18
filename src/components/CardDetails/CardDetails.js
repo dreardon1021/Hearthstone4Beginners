d35a5a4bf476faea581bf2637c064d55da00fe21
Dan Reardon <dreardon1021@gmail.com>
2020-04-17 20:18:08 -0600

    Add conditional render 4 detaials type

--- src/components/CardDetails/CardDetails.js
+++ src/components/CardDetails/CardDetails.js
@@ -3,20 +3,36 @@ import './CardDetails.css'
 import { NavLink } from 'react-router-dom'
 
 const CardDetails = props =>  {
+  let cardToRender = props.type === 'spell' ?
+    (<ul className="card-detail-list">
+    <li>Mana Cost: {props.cost}</li>
+    <li>Player Class: {props.playerClass}</li>
+    <li>Text: {props.text}</li>
+    <li>Card Type: {props.type}</li>
+    <li>Card Set: {props.cardSet}</li>
+    <li>Rarity: {props.rarity}</li>
+    <li>Flavor Text: {props.flavor}</li>
+    <li>Artist: {props.artist}</li>
+  </ul>) :
+    (<ul className="card-detail-list">
+      <li>Mana Cost: {props.cost}</li>
+      <li>Player Class: {props.playerClass}</li>
+      <li>Text: {props.text}</li>
+      <li>Attack: {props.attack}</li>
+      <li>Health: {props.health}</li>
+      <li>Card Type: {props.type}</li>
+      <li>Card Set: {props.cardSet}</li>
+      <li>Rarity: {props.rarity}</li>
+      <li>Flavor Text: {props.flavor}</li>
+      <li>Artist: {props.artist}</li>
+    </ul>)
+
   return(
     <section className="card-details">
       <h2>Card Name: {props.name}</h2>
       <div className="details-container">
         <img className="details-img" src={props.imgGold} alt={props.name}/>
-        <ul className="card-detail-list">
-          <li>Mana Cost: {props.cost}</li>
-          <li>Player Class: {props.playerClass}</li>
-          <li>Text: {props.text}</li>
-          <li>Card Type: {props.type}</li>
-          <li>Card Set: {props.cardSet}</li>
-          <li>Rarity: {props.rarity}</li>
-          <li>Flavor Text: {props.flavor}</li>
-        </ul>
+        {cardToRender}
       </div>
       <div className="details-button-container">
         <button className="details-add-btn">Add To Deck</button>
