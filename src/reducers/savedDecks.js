1f8e68e0e3bc2a77dc844d9b3c965dc16cd32a15
Dan Reardon <dreardon1021@gmail.com>
2020-04-18 16:12:50 -0600

    Add conditional to not create same deck name

--- src/reducers/savedDecks.js
+++ src/reducers/savedDecks.js
@@ -1,7 +1,21 @@
 export const savedDecks = (state = [], action) => {
   switch (action.type) {
     case 'SAVE_DECK':
-      return [...state, action.deck];
+      let incomingDeckName = Object.keys(action.deck)
+      let currentDeckNames = state.map(deck => {
+        let name = Object.keys(deck)
+        return name[0]
+      })
+      if(currentDeckNames.includes(incomingDeckName[0])) {
+        let deckToRemove = state.find(deck => {
+          let deckName = Object.keys(deck)
+          return deckName[0] === incomingDeckName[0]
+        })
+        state.splice(state.indexOf(deckToRemove), 1)
+        return [...state, action.deck]
+      } else {
+        return [...state, action.deck];
+      }
     default:
       return state;
   }
