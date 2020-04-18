0be5d007e1c2ea0bd3c4e10fccab1ae9e33031f0
Dan Reardon <dreardon1021@gmail.com>
2020-04-18 11:53:16 -0600

    Add message if deck contains no cards

--- src/components/ViewDeckContainer/ViewDeckContainer.js
+++ src/components/ViewDeckContainer/ViewDeckContainer.js
@@ -4,18 +4,24 @@ import './ViewDeckContainer.css'
 import { connect } from 'react-redux';
 
 const ViewDeckContainer = props => {
-  let cardList = props.currentDeck.map(card => {
-    let possibleCard = props.classicCards.filter(cardName => cardName.name === card)
-    let cardInfo = possibleCard.find(cardStats => cardStats.collectible === true)
-    return (<Card
-            id={cardInfo.cardId}
-            key={cardInfo.cardId}
-            name={cardInfo.name}
-            img={cardInfo.img}
-            imgGold={cardInfo.imgGold}
-            addCardToDeck={props.addCardToDeck}
-          />)
-  })
+  let cardList;
+  if(props.currentDeck.length > 0) {
+     cardList = props.currentDeck.map(card => {
+      let possibleCard = props.classicCards.filter(cardName => cardName.name === card)
+      let cardInfo = possibleCard.find(cardStats => cardStats.collectible === true)
+      return (<Card
+              id={cardInfo.cardId}
+              key={cardInfo.cardId}
+              name={cardInfo.name}
+              img={cardInfo.img}
+              imgGold={cardInfo.imgGold}
+              addCardToDeck={props.addCardToDeck}
+            />)
+    })
+  } else {
+    cardList = (<h2 className="no-card-msg">Your Deck Has No Cards</h2>)
+  }
+  
 
   return(
     <section className="card-list-container">
