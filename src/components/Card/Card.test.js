cc5ee67bd8a41e555a813b4e9c01533f1aacfb8c
Dan Reardon <dreardon1021@gmail.com>
2020-04-19 14:20:41 -0600

    Write render tests for Card and CardDetails

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Card from './Card';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux';
@@ -8,7 +8,7 @@ import rootReducer from '../../reducers';
 
 
 describe('Card', () => {
-  it('should render what we expect', () => {
+  it('should render what we expect on home', () => {
     const store = createStore(rootReducer)
 
     const {getByText, getByAltText} = render(
@@ -18,6 +18,27 @@ describe('Card', () => {
             id={1}
             name={'Circle of Healing'}
             imgGold={"http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/EX1_607_premium.gif"}
+            currentPage={"home"}
+          />
+        </Router>
+      </Provider>
+    )
+    expect(getByText('Add')).toBeInTheDocument()
+    expect(getByText('View')).toBeInTheDocument()
+    expect(getByAltText('Circle of Healing')).toBeInTheDocument()
+  })
+
+  it('should render what we expect on deck view', () => {
+    const store = createStore(rootReducer)
+
+    const {getByText, getByAltText} = render(
+      <Provider store={store}>
+        <Router>
+          <Card
+            id={1}
+            name={'Circle of Healing'}
+            imgGold={"http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/EX1_607_premium.gif"}
+            currentPage={"current-deck"}
           />
         </Router>
       </Provider>
@@ -26,4 +47,53 @@ describe('Card', () => {
     expect(getByText('View')).toBeInTheDocument()
     expect(getByAltText('Circle of Healing')).toBeInTheDocument()
   })
+
+  it('should be able to add card to deck' , () => {
+    const store = createStore(rootReducer)
+    const mockAddCardToDeck=jest.fn()
+
+    const {getByText} = render(
+      <Provider store={store}>
+        <Router>
+          <Card
+            id={1}
+            name={'Circle of Healing'}
+            imgGold={"http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/EX1_607_premium.gif"}
+            currentPage={"home"}
+            addCardToDeck={mockAddCardToDeck}
+          />
+        </Router>
+      </Provider>
+    )
+
+    fireEvent.click(getByText('Add'))
+
+    expect(mockAddCardToDeck).toHaveBeenCalledWith('Circle of Healing')
+
+  })
+
+  it('should be able to add card to deck' , () => {
+    const store = createStore(rootReducer)
+    const mockRemoveCardToDeck=jest.fn()
+
+    const {getByText} = render(
+      <Provider store={store}>
+        <Router>
+          <Card
+            id={1}
+            name={'Circle of Healing'}
+            imgGold={"http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/EX1_607_premium.gif"}
+            currentPage={"current-deck"}
+            removeCardFromDeck={mockRemoveCardToDeck}
+          />
+        </Router>
+      </Provider>
+    )
+
+    fireEvent.click(getByText('Remove'))
+
+    expect(mockRemoveCardToDeck).toHaveBeenCalledWith('Circle of Healing')
+  })
+
+
 })
