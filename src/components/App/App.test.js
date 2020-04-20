2f26388ca21df69c60ca3a7f5680a8a96834e947
Dan Reardon <dreardon1021@gmail.com>
2020-04-20 14:24:24 -0600

    Write tests for integration

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitForElement } from '@testing-library/react';
+import { render, waitForElement, fireEvent } from '@testing-library/react';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux';
@@ -15,46 +15,15 @@ describe('App', () => {
     const mockCards = [
       {
       cardId: 'EX1_144',
-      dbfId: '365',
       name: 'Shadowstep',
-      cardSet: 'Classic',
-      type: 'Spell',
-      faction: 'Neutral',
-      rarity: 'Common',
-      cost: 0,
-      text: 'Return a friendly minion to your hand. It_costs (2) less.',
-      flavor: 'Rogue dance troops will sometimes Shadowstep away at the end of a performance.  Crowds love it.',
-      artist: 'Graven Tung',
       collectible: true,
-      playerClass: 'Rogue',
-      img: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/EX1_144.png',
       imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/EX1_144_premium.gif',
-      locale: 'enUS'
     },
     {
       cardId: 'CS2_188',
-      dbfId: '242',
       name: 'Abusive Sergeant',
-      cardSet: 'Classic',
-      type: 'Minion',
-      faction: 'Alliance',
-      rarity: 'Common',
-      cost: 1,
-      attack: 1,
-      health: 1,
-      text: '<b>Battlecry:</b> Give a minion +2_Attack this turn.',
-      flavor: 'ADD ME TO YOUR DECK, MAGGOT!',
-      artist: 'Luca Zontini',
       collectible: true,
-      playerClass: 'Neutral',
-      img: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/CS2_188.png',
-      imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/CS2_188_premium.gif',
-      locale: 'enUS',
-      mechanics: [
-        {
-          name: 'Battlecry'
-        }
-      ]
+      imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/CS2_188_premium.gif'
     }
   ]
 
@@ -78,52 +47,21 @@ describe('App', () => {
     const mockCards = [
       {
       cardId: 'EX1_144',
-      dbfId: '365',
       name: 'Shadowstep',
-      cardSet: 'Classic',
-      type: 'Spell',
-      faction: 'Neutral',
-      rarity: 'Common',
-      cost: 0,
-      text: 'Return a friendly minion to your hand. It_costs (2) less.',
-      flavor: 'Rogue dance troops will sometimes Shadowstep away at the end of a performance.  Crowds love it.',
-      artist: 'Graven Tung',
       collectible: true,
-      playerClass: 'Rogue',
-      img: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/EX1_144.png',
       imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/EX1_144_premium.gif',
-      locale: 'enUS'
     },
     {
       cardId: 'CS2_188',
-      dbfId: '242',
       name: 'Abusive Sergeant',
-      cardSet: 'Classic',
-      type: 'Minion',
-      faction: 'Alliance',
-      rarity: 'Common',
-      cost: 1,
-      attack: 1,
-      health: 1,
-      text: '<b>Battlecry:</b> Give a minion +2_Attack this turn.',
-      flavor: 'ADD ME TO YOUR DECK, MAGGOT!',
-      artist: 'Luca Zontini',
       collectible: true,
-      playerClass: 'Neutral',
-      img: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/CS2_188.png',
-      imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/CS2_188_premium.gif',
-      locale: 'enUS',
-      mechanics: [
-        {
-          name: 'Battlecry'
-        }
-      ]
+      imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/CS2_188_premium.gif'
     }
   ]
 
     getClassicCards.mockResolvedValueOnce(mockCards)
 
-    const {getByText} = render(
+    const {getByText, getByTestId} = render(
       <Provider store={store}>
         <Router>
           <App/>
@@ -132,8 +70,198 @@ describe('App', () => {
     )
 
     const cardTitleEl = await waitForElement(() => getByText("Shadowstep"))
+    const addBtn = await waitForElement(() => getByTestId("Shadowstep-add"))
+    const viewBtn = await waitForElement(() => getByTestId("Shadowstep"))
 
     expect(cardTitleEl).toBeInTheDocument()
+    expect(addBtn).toBeInTheDocument()
+    expect(viewBtn).toBeInTheDocument()
+  })
+
+  it('should be able to render savedDeck List and change decks', () => {
+    const store = createStore(rootReducer,
+      {savedDecks: [{Deck1: ["Shadowstep"]}, {Deck2: ["Abusive Sergeant"]}],
+      loadCards: [
+        {
+        cardId: 'EX1_144',
+        name: 'Shadowstep',
+        collectible: true,
+        imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/EX1_144_premium.gif',
+      },
+      {
+        cardId: 'CS2_188',
+        name: 'Abusive Sergeant',
+        collectible: true,
+        imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/CS2_188_premium.gif',
+      }
+    ]})
+
+    const mockCards = [
+      {
+      cardId: 'EX1_144',
+      name: 'Shadowstep',
+      collectible: true,
+      imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/EX1_144_premium.gif',
+    },
+    {
+      cardId: 'CS2_188',
+      name: 'Abusive Sergeant',
+      collectible: true,
+      imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/CS2_188_premium.gif'
+    }
+  ]
+
+    getClassicCards.mockResolvedValueOnce(mockCards)
+
+    const {getByText, getByTestId} = render(
+      <Provider store={store}>
+        <Router>
+          <App/>
+        </Router>
+      </Provider>
+    )
+
+    fireEvent.click(getByText("Deck1"))
+
+    expect(getByTestId("Shadowstep-decklist")).toBeInTheDocument()
+
+    fireEvent.click(getByText("Deck2"))
+
+    expect(getByTestId("Abusive Sergeant-decklist")).toBeInTheDocument()
+  })
+
+  it("should be able to click on cards to view card info and go back to list", () => {
+    const store = createStore(rootReducer,
+      {savedDecks: [{Deck1: ["Shadowstep"]}, {Deck2: ["Abusive Sergeant"]}],
+      loadCards: [
+        {
+        cardId: 'EX1_144',
+        name: 'Shadowstep',
+        collectible: true,
+        imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/EX1_144_premium.gif',
+      },
+      {
+        cardId: 'CS2_188',
+        name: 'Abusive Sergeant',
+        collectible: true,
+        imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/CS2_188_premium.gif',
+      }
+    ]})
+
+    const mockCards = [
+      {
+      cardId: 'EX1_144',
+      name: 'Shadowstep',
+      collectible: true,
+      imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/EX1_144_premium.gif',
+    },
+    {
+      cardId: 'CS2_188',
+      name: 'Abusive Sergeant',
+      collectible: true,
+      imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/CS2_188_premium.gif'
+    }
+  ]
+
+    getClassicCards.mockResolvedValueOnce(mockCards)
+
+    const {getByText, getByTestId } = render(
+      <Provider store={store}>
+        <Router>
+          <App/>
+        </Router>
+      </Provider>
+    )
+
+    fireEvent.click(getByText("Deck1"))
+    fireEvent.click(getByTestId("Shadowstep-decklist"))
+
+    expect(getByText("Card Name: Shadowstep")).toBeInTheDocument() //Unique to view card page render
+
+    fireEvent.click(getByText("Back To Browse"))
+
+    expect(getByTestId("Shadowstep-add")).toBeInTheDocument() //Unuie to CardList <Card /> render
+  })
+
+  it("should be able to view current deck and go back to cardlist", () => {
+    const store = createStore(rootReducer,
+      {savedDecks: [{Deck1: ["Shadowstep"]}, {Deck2: ["Abusive Sergeant"]}],
+      loadCards: [
+        {
+        cardId: 'EX1_144',
+        name: 'Shadowstep',
+        collectible: true,
+        imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/EX1_144_premium.gif',
+      },
+      {
+        cardId: 'CS2_188',
+        name: 'Abusive Sergeant',
+        collectible: true,
+        imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/CS2_188_premium.gif',
+      }
+    ]})
+
+    const mockCards = [
+      {
+      cardId: 'EX1_144',
+      name: 'Shadowstep',
+      collectible: true,
+      imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/EX1_144_premium.gif',
+    },
+    {
+      cardId: 'CS2_188',
+      name: 'Abusive Sergeant',
+      collectible: true,
+      imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/CS2_188_premium.gif'
+    }
+  ]
+
+    getClassicCards.mockResolvedValueOnce(mockCards)
+
+    const {getByText, getByTestId} = render(
+      <Provider store={store}>
+        <Router>
+          <App/>
+        </Router>
+      </Provider>
+    )
+
+    fireEvent.click(getByText("Deck1"))
+    fireEvent.click(getByText("View Current Deck"))
+
+    expect(getByText("Back to Card List")).toBeInTheDocument()
+    expect(getByTestId("Shadowstep-rmv")).toBeInTheDocument()
+
+    fireEvent.click(getByText("Back to Card List"))
+
+    expect(getByTestId("Shadowstep-add")).toBeInTheDocument() //Unuie to CardList <Card /> render
+  })
+
+  it("can view card by clicking on a cards view btn", async () => {
+    const store = createStore(rootReducer)
+
+      const mockCards = [
+        {
+        cardId: 'EX1_144',
+        collectible: true,
+        imgGold: 'http://media.services.zam.com/v1/media/byName/hs/cards/enus/animated/EX1_144_premium.gif',
+      }
+    ]
+
+    getClassicCards.mockResolvedValueOnce(mockCards)
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <Router>
+          <App/>
+        </Router>
+      </Provider>
+    )
+
+    const viewBtn = await waitForElement(() => getByText("View"))
+    fireEvent.click(viewBtn)
+
+    expect(getByText("Card Name: Shadowstep")).toBeInTheDocument()
   })
 })
 
