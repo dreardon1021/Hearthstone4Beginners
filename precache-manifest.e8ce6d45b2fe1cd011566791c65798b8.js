bd0b8ba3bfbad24eb309eda361ddf9580571480d
Dan Reardon <dreardon1021@gmail.com>
2020-09-22 17:02:45 -0600

    Updates

---
+++ precache-manifest.e8ce6d45b2fe1cd011566791c65798b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c769f12960346fb9bbc4f15f97001f",
+    "url": "/Hearthstone4Beginners/index.html"
+  },
+  {
+    "revision": "dec229abc34f704d938a",
+    "url": "/Hearthstone4Beginners/static/css/main.8ad00709.chunk.css"
+  },
+  {
+    "revision": "c2690f9106922547854b",
+    "url": "/Hearthstone4Beginners/static/js/2.c850f6cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Hearthstone4Beginners/static/js/2.c850f6cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec229abc34f704d938a",
+    "url": "/Hearthstone4Beginners/static/js/main.fd8f0107.chunk.js"
+  },
+  {
+    "revision": "5c35ca1d8f3be56fe5f2",
+    "url": "/Hearthstone4Beginners/static/js/runtime-main.fdae59fe.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Hearthstone4Beginners/precache-manifest.e8ce6d45b2fe1cd011566791c65798b8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Hearthstone4Beginners/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
